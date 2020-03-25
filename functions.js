13f3248f768bc541971f2237520530393aecb350
hyaz <Oulephant>
2020-03-25 20:17:41 +0100

    Jquery?
    menu terminé, TODO : ajouter retractation quand clic externe

--- functions.js
+++ functions.js
@@ -1,3 +1,28 @@
+
+var state = false;
+
 function switchto(x) {
   x.classList.toggle("change");
+  if (state == false) {
+    openMenu();
+    state = true;
+  }
+  else {
+    closeMenu();
+    state = false;
+  }
 }
+
+function openMenu() {
+  document.getElementById("monmenu").style.width = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("monmenu").style.width = "0";
+}
+
+//TODO plus tard
+//https://www.w3schools.com/jquery/jquery_intro.asp
+$(document).click(function(event) {
+  if(event.target.id != 'menu' || event.target.id != 'monmenu' ) {closeMenu();}
+});
