1da8de4a06c2492cf7941b41d3ab6370d9b89e4f
hyaz <Oulephant>
2020-03-28 15:27:38 +0100

    W3c friendly
    Ajout : W3C friendly! (modif classes et id)
    DOING : Remplir site
    TODO : Vérifier responsive mobile

--- functions.js
+++ functions.js
@@ -1,6 +1,9 @@
 
 var state = false;
 var page_actuelle = ["#", "MENU","_html"];
+var tab_upper = [];
+var clicked;
+var clic;
 
 /*    ####################
       ##   JavaScript   ##
@@ -54,63 +57,79 @@ $(document).ready(function(){
   $('.menu').children().click(function(){
     //console.log(page_actuelle[0]+page_actuelle[1]+page_actuelle[2]);
     //check ancienne et nouvelle page
-    if ($(this).attr('id') != page_actuelle[1]) {
+    if ($(this).attr('class') != page_actuelle[1]) {
       //recompose classe
       $(page_actuelle[0]+page_actuelle[1]+page_actuelle[2]).hide("Drop");
-      $(page_actuelle[0]+$(this).attr('id')+page_actuelle[2]).show("Fade");
+      $(page_actuelle[0]+$(this).attr('class')+page_actuelle[2]).show("Fade");
       //remplace ancien element
-      page_actuelle[1] = $(this).attr('id');
+      page_actuelle[1] = $(this).attr('class');
       closeMenuAfter();
 
       //Bouton de retour menu
-      if ($(this).attr('id') != "MENU") {
-        $('.mini_menu').show();
+      if ($(this).attr('class') != "MENU") {
+        $('#mini_menu').show();
       }
       else {
-        $('.mini_menu').hide();
+        $('#mini_menu').hide();
       }
 
     }
   });
 
 
-  //mini_menu
+  //boutons main menu
   $('.main_buttons').click(function(){
-    //console.log(page_actuelle[0]+page_actuelle[1]+page_actuelle[2]);
+    //Recuperer clic
+    clic = $(this).attr('class');
+    //Recuperer les elements majuscules
+    for (i in clic) {
+      if (clic[i] == clic[i].toUpperCase() && clic[i] != "_" && clic[i] != " ") {
+        //Stocker
+        tab_upper.push(clic[i]);
+      }
+    }
+    //Convertion format string
+    clicked = tab_upper.join("");
+
     //check ancienne et nouvelle page
-    if ($(this).attr('id') != page_actuelle[1]) {
+    if (clicked != page_actuelle[1]) {
       //recompose classe
       $(page_actuelle[0]+page_actuelle[1]+page_actuelle[2]).hide("Drop");
-      $(page_actuelle[0]+$(this).attr('id')+page_actuelle[2]).show("Fade");
+      $(page_actuelle[0]+clicked+page_actuelle[2]).show("Fade");
       //remplace ancien element
-      page_actuelle[1] = $(this).attr('id');
+      page_actuelle[1] = clicked;
+      //Efface contenu
+      tab_upper=[];
+      
       closeMenuAfter() //Meme si pas ouvert
 
       //Bouton de retour menu
-      if ($(this).attr('id') != "MENU") {
-        $('.mini_menu').show();
+      if ($(this).attr('class') != "MENU") {
+        $('#mini_menu').show();
       }
       else {
-        $('.mini_menu').hide();
+        $('#mini_menu').hide();
       }
     }
   });
 
-  $('.mini_menu').click(function(){
-    if ($(this).attr('id') != page_actuelle[1]) {
+
+//bouton mini menu
+  $('#mini_menu').click(function(){
+    if ($(this).attr('class') != page_actuelle[1]) {
       //recompose classe
       $(page_actuelle[0]+page_actuelle[1]+page_actuelle[2]).hide("Drop");
-      $(page_actuelle[0]+$(this).attr('id')+page_actuelle[2]).show("Fade");
+      $(page_actuelle[0]+$(this).attr('class')+page_actuelle[2]).show("Fade");
       //remplace ancien element
-      page_actuelle[1] = $(this).attr('id');
+      page_actuelle[1] = $(this).attr('class');
       closeMenuAfter() //Meme si pas ouvert
 
       //Bouton de retour menu
-      if ($(this).attr('id') != "MENU") {
-        $('.mini_menu').show();
+      if ($(this).attr('class') != "MENU") {
+        $('#mini_menu').show();
       }
       else {
-        $('.mini_menu').hide();
+        $('#mini_menu').hide();
       }
     }
   });
